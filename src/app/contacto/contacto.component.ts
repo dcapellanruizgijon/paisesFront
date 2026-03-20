f6eed06588ea5f88c23f8df83f2c5d42c2a14050
dcapellanruizgijon <diego.capellan-fernandez@iesruizgijon.com>
2026-03-20 10:28:11 +0100

    modificado el componente contacto para que use validación de bootstrap (requisito de DIW)

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-nosotros',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements AfterViewInit{
 
+  //este script es cogido del ejemplo del forulario hecho en clase pero modificado para que funcione en bootstrap
+  ngAfterViewInit(): void {
+    (() => {
+      'use strict'
+      const forms = document.querySelectorAll('.needs-validation')
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          // Tipar form como HTMLFormElement para acceder a checkValidity()
+          const formElement = form as HTMLFormElement;//añadido para que funcione en angular
+          if (!formElement.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          formElement.classList.add('was-validated')
+        }, false)
+      })
+    })()
+  }
 }
